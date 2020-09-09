1ef17a54ecc6858a791762f7b20d472299fe1d2b
JinJin_Hrb <fanwang1987@gmail.com>
2020-09-10 00:56:53 +0800

    sleep after unzip

--- dist/helpers/backupServer.sh
+++ dist/helpers/backupServer.sh
@@ -7,6 +7,7 @@ str2='改前.tar.gz'
 time2=$str1$time1$str2
 tar -czvf $time2 server &&
  unzip -o server.zip -d ${destFolderPath} &&
+ sleep 5 &&
  rm -f server.zip &
  nohup forever stop index.js &
  forever start -o nohup.out -e nohup.out index.js
